31ccaa7ceb71c540f069de115b3521edb6677ed8
franciscocaballero <caballerofrancisco22@yahoo.com>
2023-01-03 18:46:23 -0500

    adding front end components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,45 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import reading from "./images/reading.jpg";
+import useStyles from "./styles";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const classes = useStyles();
 
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Posts
+        </Typography>
+        <img
+          className={classes.image}
+          src={reading}
+          alt="reading"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing="3"
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,8 @@
 import express from "express";
-import { getPosts } from "../controllers/posts.js";
-
+import { getPosts, createPost } from "../controllers/posts.js";
 const router = express.Router();
 
-// router.get("/", getPosts);
-router.get("/", (req, res) => {
-  res.send("This works");
-});
+router.get("/", getPosts);
+router.get("/", createPost);
 
 export default router;
