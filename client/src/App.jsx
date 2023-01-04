dc19f92ea800325b87ca8a498417032d1e0f6212
franciscocaballero <caballerofrancisco22@yahoo.com>
2023-01-03 22:50:47 -0500

    adding redux for state managment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
@@ -8,6 +9,7 @@ import useStyles from "./styles";
 
 function App() {
   const classes = useStyles();
+  const dispatch = useDispatch();
 
   return (
     <Container maxWidth="lg">
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
+import { applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import reducers from "./reducers";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
