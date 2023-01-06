0d9214002318bdcb346e038d70ca54543249cb4c
franciscocaballero <caballerofrancisco22@yahoo.com>
2023-01-05 21:08:45 -0500

    styling react component

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 import Post from "./Post/Post";
+import useStyles from "./styles";
 const Posts = () => {
   const posts = useSelector((state) => state.posts);
-  // const classes = useStlyes();
+  const classes = useStyles();
 
   console.log(posts);
-  return (
-    <div>
-      <h1> POSTS</h1>
-      <Post />
-      <Post />
-    </div>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid item key={post._id} xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -11,13 +11,12 @@ const CONNECTION_URL =
   "mongodb+srv://ciscocaballero:Cisco1122$@cluster0.cpq8nmo.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 3000;
 
-// app.listen(3000, () => console.log("running at 3000 port"));
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extend: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+// app.listen(3000, () => console.log("running at 3000 port"));
+app.use("/posts", postRoutes);
 
 mongoose
   .connect(CONNECTION_URL)
